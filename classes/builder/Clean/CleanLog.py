2cc48ca6e101797d91199e0006da5a1958e06a56
Bence <m.bence05@gmail.com>
2020-04-07 21:37:50 +0200

    uprava CleanLog, pridanie "\" do requests.txt

--- classes/builder/Clean/CleanLog.py
+++ classes/builder/Clean/CleanLog.py
@@ -5,7 +5,7 @@ from classes.builder.Clean.Clean import Clean
 from classes.builder.Clean.options.Robots import Robots
 from classes.builder.Clean.options.Files import Files
 from classes.builder.Clean.options.Http import Http
-from classes.builder.Clean.options.Identify import Identify
+from classes.utilities.Identify import Identify
 from classes.utilities.Path import Path
 from classes.utilities.Columns import Columns
 from classes.builder.Analyze.AnalyzeLog import AnalyzeLog
@@ -67,6 +67,63 @@ class CleanLog(Clean):
         self.__robots.set_agents_source("agents.txt")
         return self
 
+    def __is_writing_header_data(self):
+        return (self.__headers is False) \
+               and (self.__clean_up is True or self.__robots is None)
+
+    def is_not_writing_to_temp_file(self):
+        return self.__clean_up is True \
+               or self.__robots is None
+
+    def __is_generating_temp_file(self, line):
+        return line == "" \
+               and self.__clean_up is False \
+               and self.__robots is not None
+
+    def __is_about_to_finish_without_temp_file(self, line):
+        return line == "" \
+               and self.__clean_up is False \
+               and self.__robots is None
+
+    def __is_file_extension_removing_enabled(self, column_request_url):
+        return self.__files is not None \
+               and self.__files.get_extensions_source() is not None \
+               and self.__files.remove(column_request_url)
+
+    def __is_http_status_removing_enabled(self, column_http_status):
+        return self.__http is not None \
+               and self.__http.get_status_source() is not None \
+               and self.__http.remove_statuses(column_http_status)
+
+    def __is_http_method_removing_enabled(self, column_request_method):
+        return self.__http is not None \
+               and self.__http.get_method_source() is not None \
+               and self.__http.remove_methods(column_request_method)
+
+    def __is_http_request_removing_enabled(self, column_request_url):
+        return self.__http is not None\
+               and self.__http.get_request_source() is not None \
+               and self.__http.remove_request(column_request_url)
+
+    def __is_agent_removing_enabled(self, column_user_agent):
+        return self.__robots is not None \
+               and self.__robots.get_agents_source() is not None \
+               and self.__robots.remove_by_user_agent(column_user_agent)
+
+    def __is_robot_ip_blacklisting_enabled(self, column_request_url, column_ip_address):
+        return self.__robots is not None \
+               and self.__robots.black_list_ip(column_request_url, column_ip_address)
+
+    def __is_column_allowed(self, data):
+        return data.upper() not in self.__columns \
+               and (self.__clean_up is True or self.__robots is None)
+
+    def __is_about_to_finish_with_temp_file(self, line):
+        return line == "" and self.__clean_up is True
+
+    def __is_ip_removing_enabled(self, column_ip_address):
+        return self.__robots.remove_by_ip(column_ip_address)
+
     def clean_and_build(self):
         temp_file = open(Path.OUTPUT.value + "temp.log", "w", encoding="utf8")
         self.__input_file = open(Path.INPUT.value + self.__input_file_name, "r", encoding="utf8")
@@ -121,59 +178,10 @@ class CleanLog(Clean):
         return AnalyzeLog().from_file(self.__output_file_name)
 
     def __write_to_file(self, datas, file):
-        if (self.__headers is False) and (self.__clean_up is True or self.__robots is None):
+        if self.__is_writing_header_data():
             file.write('#'.join([data for data in datas if self.__is_column_allowed(data)])+"\n")
-        if self.__clean_up is True or self.__robots is None:
+        if self.is_not_writing_to_temp_file():
             file.write('#'.join([datas[data] for data in datas if self.__is_column_allowed(data)])+"\n")
         else:
             file.write(' '.join([datas[data] for data in datas if not self.__is_column_allowed(data)])+"\n")
         self.__headers = True
-
-    def __is_generating_temp_file(self, line):
-        return line == "" \
-               and self.__clean_up is False \
-               and self.__robots is not None
-
-    def __is_about_to_finish_without_temp_file(self, line):
-        return line == "" \
-               and self.__clean_up is False \
-               and self.__robots is None
-
-    def __is_file_extension_removing_enabled(self, column_request_url):
-        return self.__files is not None \
-               and self.__files.get_extensions_source() is not None \
-               and self.__files.remove(column_request_url)
-
-    def __is_http_status_removing_enabled(self, column_http_status):
-        return self.__http is not None \
-               and self.__http.get_status_source() is not None \
-               and self.__http.remove_statuses(column_http_status)
-
-    def __is_http_method_removing_enabled(self, column_request_method):
-        return self.__http is not None \
-               and self.__http.get_method_source() is not None \
-               and self.__http.remove_methods(column_request_method)
-
-    def __is_http_request_removing_enabled(self, column_request_url):
-        return self.__http is not None\
-               and self.__http.get_request_source() is not None \
-               and self.__http.remove_request(column_request_url)
-
-    def __is_agent_removing_enabled(self, column_user_agent):
-        return self.__robots is not None \
-               and self.__robots.get_agents_source() is not None \
-               and self.__robots.remove_by_user_agent(column_user_agent)
-
-    def __is_robot_ip_blacklisting_enabled(self, column_request_url, column_ip_address):
-        return self.__robots is not None \
-               and self.__robots.black_list_ip(column_request_url, column_ip_address)
-
-    def __is_column_allowed(self, data):
-        return data.upper() not in self.__columns \
-               and (self.__clean_up is True or self.__robots is None)
-
-    def __is_about_to_finish_with_temp_file(self, line):
-        return line == "" and self.__clean_up is True
-
-    def __is_ip_removing_enabled(self, column_ip_address):
-        return self.__robots.remove_by_ip(column_ip_address)
